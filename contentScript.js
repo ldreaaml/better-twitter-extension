c115476ce784c2c8c3cb3f2e5d876fd7bfe536c3
Dream Jitsawatpaiboon <dreamza43@gmail.com>
2022-12-25 01:03:54 +1300

    modify dom based on current setting

--- contentScript.js
+++ contentScript.js
@@ -1,66 +1,104 @@
 (() => {
-  const targetNode = document.querySelector("body");
-  const config = { attributes: true, childList: true, subtree: true };
-  const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      let xpath =
-        "//a[contains(@href, '/analytics')]//span[contains(text(), 'View')] | //a[contains(@aria-label, 'View Tweet analytics')]"; //hide views
-      xpath += "| //*[contains(@aria-label, 'Share Tweet')]"; //hide share buttons
-      xpath +=
-        "| //span[contains(text(), 'Promoted') and not (../@data-testid = 'tweetText')]/ancestor::article"; //hide promoted tweets
-      xpath +=
-        "| //*[local-name()='aside']//div[@role='button'  and  descendant::span[contains(text(), 'Promoted')]]"; //hide promoted accounts
-
-      let elements = document.evaluate(
-        xpath,
-        document,
-        null,
-        XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
-        null
-      );
-      let targetedElement = elements.iterateNext();
-      while (targetedElement) {
-        if (
-          targetedElement.tagName == "ARTICLE" ||
-          targetedElement.getAttribute("role") == "button"
-        ) {
-          targetedElement.style.display = "none";
+  const fetchSetting = () => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get(null, (currentSetting) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+          console.log("failed to fetch setting");
+        } else {
+          resolve(currentSetting);
         }
-        if (targetedElement.getAttribute("aria-label") == "Share Tweet") {
-          let parent = targetedElement.parentElement.parentElement;
-          if (parent) {
-            if (parent.style.display != "none") {
-              parent.style.display = "none";
+      });
+    });
+  };
+
+  const HideElements = async () => {
+    var currentSetting = await fetchSetting();
+    const targetNode = document.querySelector("body");
+    const config = { attributes: true, childList: true, subtree: true };
+    const callback = function (mutationsList, observer) {
+      for (let mutation of mutationsList) {
+        let xpath = getXpath(currentSetting);
+        let elements = document.evaluate(
+          xpath,
+          document,
+          null,
+          XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+          null
+        );
+        let targetedElement = elements.iterateNext();
+        while (targetedElement) {
+          if (
+            targetedElement.tagName == "ARTICLE" ||
+            targetedElement.getAttribute("role") == "button"
+          ) {
+            targetedElement.style.display = "none";
+          }
+          if (targetedElement.getAttribute("aria-label") == "Share Tweet") {
+            let parent = targetedElement.parentElement.parentElement;
+            if (parent) {
+              if (parent.style.display != "none") {
+                parent.style.display = "none";
+              }
+            } else {
+              if (targetedElement.style.display != "none") {
+                targetedElement.style.display = "none";
+              }
             }
           } else {
-            if (targetedElement.style.display != "none") {
-              targetedElement.style.display = "none";
+            while (targetedElement && targetedElement.tagName != "A") {
+              targetedElement = targetedElement.parentElement;
             }
-          }
-        } else {
-          while (targetedElement && targetedElement.tagName != "A") {
-            targetedElement = targetedElement.parentElement;
-          }
-          if (targetedElement) {
-            var testid = targetedElement.getAttribute("data-testid");
-            if (!testid || testid != "analyticsButton") {
-              let parent = targetedElement.parentElement;
-              if (parent) {
-                if (parent.style.display != "none") {
-                  parent.style.display = "none";
-                }
-              } else {
-                if (targetedElement.style.display != "none") {
-                  targetedElement.style.display = "none";
+            if (targetedElement) {
+              var testid = targetedElement.getAttribute("data-testid");
+              if (!testid || testid != "analyticsButton") {
+                let parent = targetedElement.parentElement;
+                if (parent) {
+                  if (parent.style.display != "none") {
+                    parent.style.display = "none";
+                  }
+                } else {
+                  if (targetedElement.style.display != "none") {
+                    targetedElement.style.display = "none";
+                  }
                 }
               }
             }
           }
+          targetedElement = elements.iterateNext();
         }
-        targetedElement = elements.iterateNext();
       }
-    }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
   };
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+
+  HideElements();
 })();
+
+const getXpath = (currentSetting) => {
+  let viewXPath =
+    "//a[contains(@href, '/analytics')]//span[contains(text(), 'View')] | //a[contains(@aria-label, 'View Tweet analytics')]"; //hide views
+  let shareXPath = "//*[contains(@aria-label, 'Share Tweet')]";
+  let promotedTweetXPath =
+    "//span[contains(text(), 'Promoted') and not (../@data-testid = 'tweetText')]/ancestor::article"; //hide promoted tweets
+  let promotedAccountXPath =
+    "//*[local-name()='aside']//div[@role='button'  and  descendant::span[contains(text(), 'Promoted')]]"; //hide promoted accounts
+
+  var xpath = [];
+  if (currentSetting.view) {
+    xpath.push(viewXPath);
+  }
+  if (currentSetting.share) {
+    xpath.push(shareXPath);
+  }
+  if (currentSetting.promotedTweet) {
+    xpath.push(promotedTweetXPath);
+  }
+  if (currentSetting.promotedAccount) {
+    xpath.push(promotedAccountXPath);
+  }
+
+  xpath = xpath.join(" | ");
+  return xpath;
+};
